b25f0b7fc305b3f744c16e93bebae8ebd07aa58b
Umiusi1219 <https://github.com/Umiusi1219>
2022-05-20 18:11:11 +0900

    レーンを4レーンに増設

--- VALIDSENSE2022/Assets/Chan/MainGame/Notes/ViewNoteCreater.cs
+++ VALIDSENSE2022/Assets/Chan/MainGame/Notes/ViewNoteCreater.cs
@@ -4,28 +4,39 @@ using UnityEngine;
 
 public class ViewNoteCreater : MonoBehaviour
 {
-    [SerializeField] List<GameObject> viewNotes;
+    [SerializeField] 
+    List<GameObject> viewNotes;
 
-    [SerializeField] List<GameObject> line0ViewNoteList;
-    [SerializeField] List<GameObject> line1ViewNoteList;
-    //[SerializeField] List<GameObject> line0ViewNoteList;
-    //[SerializeField] List<GameObject> line0ViewNoteList;
+    [SerializeField] 
+    List<GameObject> line0ViewNoteList;
+    [SerializeField] 
+    List<GameObject> line1ViewNoteList;
+    [SerializeField] 
+    List<GameObject> line2ViewNoteList;
+    [SerializeField] 
+    List<GameObject> line3ViewNoteList;
 
+    [SerializeField] 
     public int[] lineViewNoteNum ;
 
+    [SerializeField] 
     public int[] lineViewNoteDereatNum;
 
-    [SerializeField] private float instantiatePosY;
+    [SerializeField] 
+    private float instantiatePosY;
 
-    [SerializeField] private float[] instantiatePosX;
+    [SerializeField] 
+    private float[] instantiatePosX;
 
-    [SerializeField] long viewNoteTime;
+    [SerializeField,Range(0,9000)] 
+    long viewNoteTime = 4500;
 
+    [Range(0.5f,30.0f)]
     public float viewNoteSpeed;
 
     private void FixedUpdate()
     {
-        if (NoteDataListCreater.listNumMax0 > lineViewNoteNum[0])
+        if (NoteDataListCreater.listNumMax[0] > lineViewNoteNum[0])
         {
             if((NoteDataListCreater.noteList_0[lineViewNoteNum[0]].time - viewNoteTime) <= MusicData.Timer)
             {
@@ -41,7 +52,8 @@ public class ViewNoteCreater : MonoBehaviour
 
                 //line0ViewNoteList.Add(viewNotes[NoteDataListCreater.noteList_0[lineViewNoteNum[0]].type]);
 
-                line0ViewNoteList.Add(Instantiate(viewNotes[0],
+                line0ViewNoteList.Add(
+                    Instantiate(viewNotes[NoteDataListCreater.noteList_0[lineViewNoteNum[0]].type],
                     new Vector3(instantiatePosX[NoteDataListCreater.noteList_0[lineViewNoteNum[0]].line], instantiatePosY * viewNoteSpeed, 87.5f),
                     new Quaternion(0, 0, 0, 0)) as GameObject);
                 line0ViewNoteList[lineViewNoteNum[0]].GetComponent<NoteView>().NoteSpeedSetter((instantiatePosY * viewNoteSpeed) / (viewNoteTime / 1000f) / 60);
@@ -51,7 +63,7 @@ public class ViewNoteCreater : MonoBehaviour
             }
         }
 
-        if (NoteDataListCreater.listNumMax1 > lineViewNoteNum[1])
+        if (NoteDataListCreater.listNumMax[1] > lineViewNoteNum[1])
         {
             if ((NoteDataListCreater.noteList_1[lineViewNoteNum[1]].time - viewNoteTime) <= MusicData.Timer)
             {
@@ -67,13 +79,74 @@ public class ViewNoteCreater : MonoBehaviour
 
                 //line1ViewNoteList.Add(viewNotes[NoteDataListCreater.noteList_1[lineViewNoteNum[1]].type]);
 
-                line1ViewNoteList.Add(Instantiate(viewNotes[1],
+                line1ViewNoteList.Add(
+                    Instantiate(viewNotes[NoteDataListCreater.noteList_1[lineViewNoteNum[1]].type],
                     new Vector3(instantiatePosX[NoteDataListCreater.noteList_1[lineViewNoteNum[1]].line], instantiatePosY * viewNoteSpeed, 87.5f),
                     new Quaternion(0, 0, 0, 0)) as GameObject);
                 line1ViewNoteList[lineViewNoteNum[1]].GetComponent<NoteView>().NoteSpeedSetter((instantiatePosY * viewNoteSpeed) / (viewNoteTime / 1000f) / 60);
 
 
                 lineViewNoteNum[1]++;
+            }
+        }
+
+        if (NoteDataListCreater.listNumMax[2] > lineViewNoteNum[2])
+        {
+            if ((NoteDataListCreater.noteList_2[lineViewNoteNum[2]].time - viewNoteTime) <= MusicData.Timer)
+            {
+                Debug.Log("noteList_1[line1ViewNoteNum].time - viewNoteTime");
+                Debug.Log(NoteDataListCreater.noteList_2[lineViewNoteNum[2]].time - viewNoteTime);
+                Debug.Log("NoteCreateTime");
+                Debug.Log(MusicData.Timer);
+                Debug.Log("noteList_1[line1ViewNoteNum].time");
+                Debug.Log(NoteDataListCreater.noteList_2[lineViewNoteNum[2]].time);
+
+                Debug.Log("noteList_1[line1ViewNoteNum].type");
+                Debug.Log(NoteDataListCreater.noteList_2[lineViewNoteNum[2]].type);
+
+                //line1ViewNoteList.Add(viewNotes[NoteDataListCreater.noteList_1[lineViewNoteNum[1]].type]);
+
+                line2ViewNoteList.Add(
+                    Instantiate(viewNotes[NoteDataListCreater.noteList_2[lineViewNoteNum[2]].type],
+                    new Vector3(instantiatePosX[NoteDataListCreater.
+                    noteList_2[lineViewNoteNum[2]].line], instantiatePosY * viewNoteSpeed, 87.5f),
+                    new Quaternion(0, 0, 0, 0)) as GameObject);
+
+                line2ViewNoteList[lineViewNoteNum[2]].GetComponent<NoteView>().
+                    NoteSpeedSetter((instantiatePosY * viewNoteSpeed) / (viewNoteTime / 1000f) / 60);
+
+
+                lineViewNoteNum[2]++;
+            }
+        }
+
+        if (NoteDataListCreater.listNumMax[3] > lineViewNoteNum[3])
+        {
+            if ((NoteDataListCreater.noteList_3[lineViewNoteNum[3]].time - viewNoteTime) <= MusicData.Timer)
+            {
+                Debug.Log("noteList_1[line1ViewNoteNum].time - viewNoteTime");
+                Debug.Log(NoteDataListCreater.noteList_3[lineViewNoteNum[3]].time - viewNoteTime);
+                Debug.Log("NoteCreateTime");
+                Debug.Log(MusicData.Timer);
+                Debug.Log("noteList_1[line1ViewNoteNum].time");
+                Debug.Log(NoteDataListCreater.noteList_3[lineViewNoteNum[3]].time);
+
+                Debug.Log("noteList_1[line1ViewNoteNum].type");
+                Debug.Log(NoteDataListCreater.noteList_3[lineViewNoteNum[3]].type);
+
+                //line1ViewNoteList.Add(viewNotes[NoteDataListCreater.noteList_1[lineViewNoteNum[1]].type]);
+
+                line3ViewNoteList.Add(
+                    Instantiate(viewNotes[NoteDataListCreater.noteList_3[lineViewNoteNum[3]].type],
+                    new Vector3(instantiatePosX[NoteDataListCreater.
+                    noteList_3[lineViewNoteNum[3]].line], instantiatePosY * viewNoteSpeed, 87.5f),
+                    new Quaternion(0, 0, 0, 0)) as GameObject);
+
+                line3ViewNoteList[lineViewNoteNum[3]].GetComponent<NoteView>().
+                    NoteSpeedSetter((instantiatePosY * viewNoteSpeed) / (viewNoteTime / 1000f) / 60);
+
+
+                lineViewNoteNum[3]++;
             }
         }
 
@@ -94,6 +167,18 @@ public class ViewNoteCreater : MonoBehaviour
                 Debug.Log(lineViewNoteDereatNum[1]);
                 line1ViewNoteList[lineViewNoteDereatNum[1]].GetComponent<NoteView>().NoteFadeOut();
                 lineViewNoteDereatNum[1]++;
+                break;
+
+            case 2:
+                Debug.Log(lineViewNoteDereatNum[2]);
+                line2ViewNoteList[lineViewNoteDereatNum[2]].GetComponent<NoteView>().NoteFadeOut();
+                lineViewNoteDereatNum[2]++;
+                break;
+
+            case 3:
+                Debug.Log(lineViewNoteDereatNum[3]);
+                line3ViewNoteList[lineViewNoteDereatNum[3]].GetComponent<NoteView>().NoteFadeOut();
+                lineViewNoteDereatNum[3]++;
                 break;
         }
     }
